181a382e5117e20f1f418c93d15e0c6eea319653
verdantjuly <131671804+verdantjuly@users.noreply.github.com>
2023-07-10 09:03:31 +0900

    style : annotation removed
    to speed up

--- repositories/likes.repository.js
+++ repositories/likes.repository.js
@@ -16,35 +16,7 @@ class LikeRepository {
       return like;
     }
   };
-  // likeslist = async (userId) => {
-  //   const target = await Likes.findAll({ where: { userId } });
-  //   const postIds = target.map((like) => {
-  //     return like.postId;
-  //   });
-  //   const allpost = await Posts.findAll({
-  //     include: [
-  //       {
-  //         model: Users,
-  //         attributes: ["nickname"],
-  //       },
-  //     ],
-  //   });
-  //   const result = allpost
-  //     .map(async (post) => {
-  //       const likesCount = await Likes.count({
-  //         where: { postId: post.postId },
-  //       });
-  //       post.likesCount = likesCount;
-  //       if (postIds.includes(post.postId)) {
-  //         return post;
-  //       } else {
-  //         return 0;
-  //       }
-  //     })
-  //     .filter((item) => item.postId == true);
-  //   const answer = await Promise.all(result);
-  //   return answer;
-  // };
+ 
   likeslist = async (userId) => {
     const target = await Likes.findAll({ where: { userId } });
     const postIds = target.map((like) => {
